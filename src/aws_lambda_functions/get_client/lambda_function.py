6da791ae821b14d6187973a4cb3ba05978e1648b
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2021-02-17 16:55:12 +0600

    BT-486

--- src/aws_lambda_functions/get_client/lambda_function.py
+++ src/aws_lambda_functions/get_client/lambda_function.py
@@ -1,38 +1,117 @@
+import logging
+import os
+import uuid
+from psycopg2.extras import RealDictCursor
+from functools import wraps
+from typing import *
+from threading import Thread
+from queue import Queue
 import databases
 import utils
-import logging
-import sys
-import os
-from psycopg2.extras import RealDictCursor
 
-"""
-Define the connection to the database outside of the "lambda_handler" function.
-The connection to the database will be created the first time the function is called.
-Any subsequent function call will use the same database connection.
-"""
-postgresql_connection = None
+# Configure the logging tool in the AWS Lambda function.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
 
-# Define databases settings parameters.
+# Initialize constants with parameters to configure.
 POSTGRESQL_USERNAME = os.environ["POSTGRESQL_USERNAME"]
 POSTGRESQL_PASSWORD = os.environ["POSTGRESQL_PASSWORD"]
 POSTGRESQL_HOST = os.environ["POSTGRESQL_HOST"]
 POSTGRESQL_PORT = int(os.environ["POSTGRESQL_PORT"])
 POSTGRESQL_DB_NAME = os.environ["POSTGRESQL_DB_NAME"]
 
-logger = logging.getLogger(__name__)  # Create the logger with the specified name.
-logger.setLevel(logging.WARNING)  # Set the logging level of the logger.
+# The connection to the database will be created the first time the AWS Lambda function is called.
+# Any subsequent call to the function will use the same database connection until the container stops.
+POSTGRESQL_CONNECTION = None
 
 
-def lambda_handler(event, context):
-    """
-    :argument event: The AWS Lambda uses this parameter to pass in event data to the handler.
-    :argument context: The AWS Lambda uses this parameter to provide runtime information to your handler.
-    """
-    # Since the connection with the database were defined outside of the function, we create global variable.
-    global postgresql_connection
-    if not postgresql_connection:
+def run_multithreading_tasks(functions: List[Dict[AnyStr, Union[Callable, Dict[AnyStr, Any]]]]) -> Dict[AnyStr, Any]:
+    # Create the empty list to save all parallel threads.
+    threads = []
+
+    # Create the queue to store all results of functions.
+    queue = Queue()
+
+    # Create the thread for each function.
+    for function in functions:
+        # Check whether the input arguments have keys in their dictionaries.
         try:
-            postgresql_connection = databases.create_postgresql_connection(
+            function_object = function["function_object"]
+        except KeyError as error:
+            logger.error(error)
+            raise Exception(error)
+        try:
+            function_arguments = function["function_arguments"]
+        except KeyError as error:
+            logger.error(error)
+            raise Exception(error)
+
+        # Add the instance of the queue to the list of function arguments.
+        function_arguments["queue"] = queue
+
+        # Create the thread.
+        thread = Thread(target=function_object, kwargs=function_arguments)
+        threads.append(thread)
+
+    # Start all parallel threads.
+    for thread in threads:
+        thread.start()
+
+    # Wait until all parallel threads are finished.
+    for thread in threads:
+        thread.join()
+
+    # Get the results of all threads.
+    results = {}
+    while not queue.empty():
+        results = {**results, **queue.get()}
+
+    # Return the results of all threads.
+    return results
+
+
+def check_input_arguments(**kwargs) -> None:
+    # Make sure that all the necessary arguments for the AWS Lambda function are present.
+    try:
+        input_arguments = kwargs["event"]["arguments"]["input"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+    try:
+        queue = kwargs["queue"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Check the format and values of required arguments in the list of input arguments.
+    required_arguments = ["userId"]
+    for argument_name, argument_value in input_arguments.items():
+        if argument_name not in required_arguments:
+            raise Exception("The '{0}' argument doesn't exist.".format(argument_name))
+        if argument_value is None:
+            raise Exception("The '{0}' argument can't be None/Null/Undefined.".format(argument_name))
+        if argument_name == "userId":
+            try:
+                uuid.UUID(argument_value)
+            except ValueError:
+                raise Exception("The '{0}' argument format is not UUID.".format(argument_name))
+
+    # Put the result of the function in the queue.
+    queue.put({
+        "input_arguments": {
+            "user_id": input_arguments["userId"]
+        }
+    })
+
+    # Return nothing.
+    return None
+
+
+def reuse_or_recreate_postgresql_connection(queue: Queue) -> None:
+    global POSTGRESQL_CONNECTION
+    if not POSTGRESQL_CONNECTION:
+        try:
+            POSTGRESQL_CONNECTION = databases.create_postgresql_connection(
                 POSTGRESQL_USERNAME,
                 POSTGRESQL_PASSWORD,
                 POSTGRESQL_HOST,
@@ -41,64 +120,88 @@ def lambda_handler(event, context):
             )
         except Exception as error:
             logger.error(error)
-            sys.exit(1)
+            raise Exception("Unable to connect to the PostgreSQL database.")
+    queue.put({"postgresql_connection": POSTGRESQL_CONNECTION})
+    return None
 
-    # Define the value of the data passed to the function.
-    user_id = event["arguments"]["userId"]
 
-    # With a dictionary cursor, the data is sent in a form of Python dictionaries.
-    cursor = postgresql_connection.cursor(cursor_factory=RealDictCursor)
+def postgresql_wrapper(function):
+    @wraps(function)
+    def wrapper(**kwargs):
+        try:
+            postgresql_connection = kwargs["postgresql_connection"]
+        except KeyError as error:
+            logger.error(error)
+            raise Exception(error)
+        cursor = postgresql_connection.cursor(cursor_factory=RealDictCursor)
+        kwargs["cursor"] = cursor
+        result = function(**kwargs)
+        cursor.close()
+        return result
+    return wrapper
 
-    # Prepare the SQL request that returns the information of the specific client.
-    statement = """
+
+@postgresql_wrapper
+def get_client_data(**kwargs) -> Any:
+    # Check if the input dictionary has all the necessary keys.
+    try:
+        cursor = kwargs["cursor"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+    try:
+        sql_arguments = kwargs["sql_arguments"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Prepare the SQL request that return information about the client.
+    sql_statement = """
     select
-        users.user_id,
-        users.entry_created_date_time as created_date_time,
         case
             when users.identified_user_id is not null and users.unidentified_user_id is null
-            then identified_users.identified_user_first_name
+            then 'identified_user'::text
+            else 'unidentified_user'::text
+        end as user_type,
+        users.user_id::text,
+        users.user_nickname::text,
+        users.user_profile_photo_url::text,
+        users.entry_created_date_time::text as created_date_time,
+        case
+            when users.identified_user_id is not null and users.unidentified_user_id is null
+            then identified_users.identified_user_first_name::text
             else null
         end as user_first_name,
         case
             when users.identified_user_id is not null and users.unidentified_user_id is null
-            then identified_users.identified_user_last_name
+            then identified_users.identified_user_last_name::text
             else null
         end as user_last_name,
         case
             when users.identified_user_id is not null and users.unidentified_user_id is null
-            then identified_users.identified_user_middle_name
+            then identified_users.identified_user_middle_name::text
             else null
         end as user_middle_name,
         case
             when users.identified_user_id is not null and users.unidentified_user_id is null
-            then identified_users.identified_user_primary_email
+            then identified_users.identified_user_primary_email::text
             else null
         end as user_primary_email,
         case
             when users.identified_user_id is not null and users.unidentified_user_id is null
-            then identified_users.identified_user_secondary_email
+            then identified_users.identified_user_secondary_email::text[]
             else null
         end as user_secondary_email,
         case
             when users.identified_user_id is not null and users.unidentified_user_id is null
-            then identified_users.identified_user_primary_phone_number
+            then identified_users.identified_user_primary_phone_number::text
             else null
         end as user_primary_phone_number,
         case
             when users.identified_user_id is not null and users.unidentified_user_id is null
-            then identified_users.identified_user_secondary_phone_number
+            then identified_users.identified_user_secondary_phone_number::text[]
             else null
         end as user_secondary_phone_number,
-        case
-            when users.identified_user_id is not null and users.unidentified_user_id is null
-            then identified_users.identified_user_profile_photo_url
-            else null
-        end as user_profile_photo_url,
-        case
-            when users.identified_user_id is not null and users.unidentified_user_id is null
-            then 'identified_user'
-            else 'unidentified_user'
-        end as user_type,
         case
             when users.identified_user_id is not null and users.unidentified_user_id is null
             then identified_users.metadata::text
@@ -106,22 +209,22 @@ def lambda_handler(event, context):
         end as metadata,
         case
             when users.identified_user_id is not null
-            and users.unidentified_user_id is null then identified_users.telegram_username
+            and users.unidentified_user_id is null then identified_users.telegram_username::text
             else null
         end as telegram_username,
         case
             when users.identified_user_id is not null
-            and users.unidentified_user_id is null then identified_users.whatsapp_profile
+            and users.unidentified_user_id is null then identified_users.whatsapp_profile::text
             else null
         end as whatsapp_profile,
         case
             when users.identified_user_id is not null
-            and users.unidentified_user_id is null then identified_users.whatsapp_username
+            and users.unidentified_user_id is null then identified_users.whatsapp_username::text
             else null
         end as whatsapp_username,
-        genders.gender_id,
-        genders.gender_technical_name,
-        genders.gender_public_name
+        genders.gender_id::text,
+        genders.gender_technical_name::text,
+        genders.gender_public_name::text
     from
         users
     left join identified_users on
@@ -131,38 +234,80 @@ def lambda_handler(event, context):
     left join genders on
         identified_users.gender_id = genders.gender_id
     where
-        users.user_id = '{0}'
+        users.user_id = %(user_id)s
     limit 1;
-    """.format(user_id)
+    """
 
-    # Execute a previously prepared SQL query.
+    # Execute the SQL query dynamically, in a convenient and safe way.
     try:
-        cursor.execute(statement)
+        cursor.execute(sql_statement, sql_arguments)
     except Exception as error:
         logger.error(error)
-        sys.exit(1)
+        raise Exception(error)
 
-    # After the successful execution of the query commit your changes to the database.
-    postgresql_connection.commit()
+    # Return the information of the client.
+    return cursor.fetchone()
 
-    # Fetch the next row of a query result set.
-    client_entry = cursor.fetchone()
 
-    # The cursor will be unusable from this point forward.
-    cursor.close()
+def analyze_and_format_client_data(**kwargs) -> Any:
+    # Check if the input dictionary has all the necessary keys.
+    try:
+        client_data = kwargs["client_data"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
 
-    # Analyze the data about client received from the database.
-    client = dict()
-    if client_entry is not None:
-        gender = dict()
-        for key, value in client_entry.items():
-            if ("_id" in key or "_date_time" in key) and value is not None:
-                value = str(value)
-            if "gender_" in key:
+    # Format the client data.
+    client = {}
+    if client_data is not None:
+        gender = {}
+        for key, value in client_data.items():
+            if key.startswith("gender_"):
                 gender[utils.camel_case(key)] = value
             else:
                 client[utils.camel_case(key)] = value
         client["gender"] = gender
 
-    # Return the full information about the client as the response.
+    # Return the information of the client.
+    return client
+
+
+def lambda_handler(event, context):
+    """
+    :param event: The AWS Lambda function uses this parameter to pass in event data to the handler.
+    :param context: The AWS Lambda function uses this parameter to provide runtime information to your handler.
+    """
+    # Run several initialization functions in parallel.
+    results_of_tasks = run_multithreading_tasks([
+        {
+            "function_object": check_input_arguments,
+            "function_arguments": {
+                "event": event
+            }
+        },
+        {
+            "function_object": reuse_or_recreate_postgresql_connection,
+            "function_arguments": {}
+        }
+    ])
+
+    # Define the input arguments of the AWS Lambda function.
+    input_arguments = results_of_tasks["input_arguments"]
+    user_id = input_arguments["user_id"]
+
+    # Define the instances of the database connections.
+    postgresql_connection = results_of_tasks["postgresql_connection"]
+
+    # Get information of the client.
+    client_data = get_client_data(
+        postgresql_connection=postgresql_connection,
+        sql_arguments={
+            "user_id": user_id
+        }
+    )
+
+    # Define variables that stores formatted information about client.
+    client = analyze_and_format_client_data(client_data=client_data)
+
+    # Return the information of the client.
     return client
--- src/aws_lambda_functions/get_internal_user/lambda_function.py
+++ src/aws_lambda_functions/get_internal_user/lambda_function.py
@@ -1,38 +1,111 @@
-import databases
-import utils
 import logging
-import sys
 import os
 from psycopg2.extras import RealDictCursor
+from functools import wraps
+from typing import *
+from threading import Thread
+from queue import Queue
+import databases
+import utils
 
-"""
-Define the connection to the database outside of the "lambda_handler" function.
-The connection to the database will be created the first time the function is called.
-Any subsequent function call will use the same database connection.
-"""
-postgresql_connection = None
+# Configure the logging tool in the AWS Lambda function.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
 
-# Define databases settings parameters.
+# Initialize constants with parameters to configure.
 POSTGRESQL_USERNAME = os.environ["POSTGRESQL_USERNAME"]
 POSTGRESQL_PASSWORD = os.environ["POSTGRESQL_PASSWORD"]
 POSTGRESQL_HOST = os.environ["POSTGRESQL_HOST"]
 POSTGRESQL_PORT = int(os.environ["POSTGRESQL_PORT"])
 POSTGRESQL_DB_NAME = os.environ["POSTGRESQL_DB_NAME"]
 
-logger = logging.getLogger(__name__)  # Create the logger with the specified name.
-logger.setLevel(logging.WARNING)  # Set the logging level of the logger.
+# The connection to the database will be created the first time the AWS Lambda function is called.
+# Any subsequent call to the function will use the same database connection until the container stops.
+POSTGRESQL_CONNECTION = None
 
 
-def lambda_handler(event, context):
-    """
-    :argument event: The AWS Lambda uses this parameter to pass in event data to the handler.
-    :argument context: The AWS Lambda uses this parameter to provide runtime information to your handler.
-    """
-    # Since the connection with the database were defined outside of the function, we create global variable.
-    global postgresql_connection
-    if not postgresql_connection:
+def run_multithreading_tasks(functions: List[Dict[AnyStr, Union[Callable, Dict[AnyStr, Any]]]]) -> Dict[AnyStr, Any]:
+    # Create the empty list to save all parallel threads.
+    threads = []
+
+    # Create the queue to store all results of functions.
+    queue = Queue()
+
+    # Create the thread for each function.
+    for function in functions:
+        # Check whether the input arguments have keys in their dictionaries.
         try:
-            postgresql_connection = databases.create_postgresql_connection(
+            function_object = function["function_object"]
+        except KeyError as error:
+            logger.error(error)
+            raise Exception(error)
+        try:
+            function_arguments = function["function_arguments"]
+        except KeyError as error:
+            logger.error(error)
+            raise Exception(error)
+
+        # Add the instance of the queue to the list of function arguments.
+        function_arguments["queue"] = queue
+
+        # Create the thread.
+        thread = Thread(target=function_object, kwargs=function_arguments)
+        threads.append(thread)
+
+    # Start all parallel threads.
+    for thread in threads:
+        thread.start()
+
+    # Wait until all parallel threads are finished.
+    for thread in threads:
+        thread.join()
+
+    # Get the results of all threads.
+    results = {}
+    while not queue.empty():
+        results = {**results, **queue.get()}
+
+    # Return the results of all threads.
+    return results
+
+
+def check_input_arguments(**kwargs) -> None:
+    # Make sure that all the necessary arguments for the AWS Lambda function are present.
+    try:
+        input_arguments = kwargs["event"]["arguments"]["input"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+    try:
+        queue = kwargs["queue"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Check the format and values of required arguments in the list of input arguments.
+    required_arguments = ["auth0UserId"]
+    for argument_name, argument_value in input_arguments.items():
+        if argument_name not in required_arguments:
+            raise Exception("The '{0}' argument doesn't exist.".format(argument_name))
+        if argument_value is None:
+            raise Exception("The '{0}' argument can't be None/Null/Undefined.".format(argument_name))
+
+    # Put the result of the function in the queue.
+    queue.put({
+        "input_arguments": {
+            "auth0_user_id": input_arguments["auth0UserId"]
+        }
+    })
+
+    # Return nothing.
+    return None
+
+
+def reuse_or_recreate_postgresql_connection(queue: Queue) -> None:
+    global POSTGRESQL_CONNECTION
+    if not POSTGRESQL_CONNECTION:
+        try:
+            POSTGRESQL_CONNECTION = databases.create_postgresql_connection(
                 POSTGRESQL_USERNAME,
                 POSTGRESQL_PASSWORD,
                 POSTGRESQL_HOST,
@@ -41,28 +114,56 @@ def lambda_handler(event, context):
             )
         except Exception as error:
             logger.error(error)
-            sys.exit(1)
+            raise Exception("Unable to connect to the PostgreSQL database.")
+    queue.put({"postgresql_connection": POSTGRESQL_CONNECTION})
+    return None
 
-    # Define the value of the data passed to the function.
-    auth0_user_id = event["arguments"]["auth0UserId"]
 
-    # With a dictionary cursor, the data is sent in a form of Python dictionaries.
-    cursor = postgresql_connection.cursor(cursor_factory=RealDictCursor)
+def postgresql_wrapper(function):
+    @wraps(function)
+    def wrapper(**kwargs):
+        try:
+            postgresql_connection = kwargs["postgresql_connection"]
+        except KeyError as error:
+            logger.error(error)
+            raise Exception(error)
+        cursor = postgresql_connection.cursor(cursor_factory=RealDictCursor)
+        kwargs["cursor"] = cursor
+        result = function(**kwargs)
+        cursor.close()
+        return result
+    return wrapper
 
-    # Prepare the SQL request that returns all detailed information about specific internal user.
-    statement = """
+
+@postgresql_wrapper
+def get_internal_user_data(**kwargs) -> Any:
+    # Check if the input dictionary has all the necessary keys.
+    try:
+        cursor = kwargs["cursor"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+    try:
+        sql_arguments = kwargs["sql_arguments"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Prepare the SQL request that return information about new created internal user.
+    sql_statement = """
     select
-        internal_users.auth0_user_id,
+        internal_users.auth0_user_id::text,
         internal_users.auth0_metadata::text,
-        users.user_id,
-        internal_users.internal_user_first_name as user_first_name,
-        internal_users.internal_user_last_name as user_last_name,
-        internal_users.internal_user_middle_name as user_middle_name,
-        internal_users.internal_user_primary_email as user_primary_email,
-        internal_users.internal_user_secondary_email as user_secondary_email,
-        internal_users.internal_user_primary_phone_number as user_primary_phone_number,
-        internal_users.internal_user_secondary_phone_number as user_secondary_phone_number,
-        internal_users.internal_user_profile_photo_url as user_profile_photo_url,
+        users.user_id::text,
+        users.user_nickname::text,
+        users.user_profile_photo_url::text,
+        internal_users.internal_user_first_name::text as user_first_name,
+        internal_users.internal_user_last_name::text as user_last_name,
+        internal_users.internal_user_middle_name::text as user_middle_name,
+        internal_users.internal_user_primary_email::text as user_primary_email,
+        internal_users.internal_user_secondary_email::text[] as user_secondary_email,
+        internal_users.internal_user_primary_phone_number::text as user_primary_phone_number,
+        internal_users.internal_user_secondary_phone_number::text[] as user_secondary_phone_number,
         internal_users.internal_user_position_name as user_position_name,
         genders.gender_id,
         genders.gender_technical_name,
@@ -91,42 +192,41 @@ def lambda_handler(event, context):
     left join organizations on
         internal_users.organization_id = organizations.organization_id
     where
-        internal_users.auth0_user_id = '{0}'
+        internal_users.auth0_user_id = %(auth0_user_id)s
     and
         users.internal_user_id is not null
     limit 1;
-    """.format(auth0_user_id)
+    """
 
-    # Execute a previously prepared SQL query.
+    # Execute the SQL query dynamically, in a convenient and safe way.
     try:
-        cursor.execute(statement)
+        cursor.execute(sql_statement, sql_arguments)
     except Exception as error:
         logger.error(error)
-        sys.exit(1)
+        raise Exception(error)
 
-    # After the successful execution of the query commit your changes to the database.
-    postgresql_connection.commit()
+    # Return the information of the new created internal user.
+    return cursor.fetchone()
 
-    # Fetch the next row of a query result set.
-    internal_user_entry = cursor.fetchone()
 
-    # The cursor will be unusable from this point forward.
-    cursor.close()
+def analyze_and_format_internal_user_data(**kwargs) -> Any:
+    # Check if the input dictionary has all the necessary keys.
+    try:
+        internal_user_data = kwargs["internal_user_data"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
 
-    # Analyze the data about internal user received from the database.
-    internal_user = dict()
-    if internal_user_entry is not None:
-        gender = dict()
-        role = dict()
-        organization = dict()
-        for key, value in internal_user_entry.items():
-            if "_id" in key and value is not None:
-                value = str(value)
-            if "gender_" in key:
+    # Format the internal user data.
+    internal_user = {}
+    if internal_user_data is not None:
+        gender, role, organization = {}, {}, {}
+        for key, value in internal_user_data.items():
+            if key.startswith("gender_"):
                 gender[utils.camel_case(key)] = value
-            elif "role_" in key:
+            elif key.startswith("role_"):
                 role[utils.camel_case(key)] = value
-            elif "organization_" in key:
+            elif key.startswith("organization_"):
                 organization[utils.camel_case(key)] = value
             else:
                 internal_user[utils.camel_case(key)] = value
@@ -134,5 +234,46 @@ def lambda_handler(event, context):
         internal_user["role"] = role
         internal_user["organization"] = organization
 
-    # Return the full information about the internal user as the response.
+    # Return the information of the new created internal user.
+    return internal_user
+
+
+def lambda_handler(event, context):
+    """
+    :param event: The AWS Lambda function uses this parameter to pass in event data to the handler.
+    :param context: The AWS Lambda function uses this parameter to provide runtime information to your handler.
+    """
+    # Run several initialization functions in parallel.
+    results_of_tasks = run_multithreading_tasks([
+        {
+            "function_object": check_input_arguments,
+            "function_arguments": {
+                "event": event
+            }
+        },
+        {
+            "function_object": reuse_or_recreate_postgresql_connection,
+            "function_arguments": {}
+        }
+    ])
+
+    # Define the input arguments of the AWS Lambda function.
+    input_arguments = results_of_tasks["input_arguments"]
+    auth0_user_id = input_arguments["auth0_user_id"]
+
+    # Define the instances of the database connections.
+    postgresql_connection = results_of_tasks["postgresql_connection"]
+
+    # Get information of the internal user.
+    internal_user_data = get_internal_user_data(
+        postgresql_connection=postgresql_connection,
+        sql_arguments={
+            "auth0_user_id": auth0_user_id
+        }
+    )
+
+    # Define variables that stores formatted information about internal user.
+    internal_user = analyze_and_format_internal_user_data(internal_user_data=internal_user_data)
+
+    # Return the information of the internal user.
     return internal_user
